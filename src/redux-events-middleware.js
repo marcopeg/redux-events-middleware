c891982d3231ffb1a88fd94f7c49530cd7a61e13
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-05 11:28:52 +0200

    transpile

---
+++ src/redux-events-middleware.js
@@ -0,0 +1,45 @@
+/*
+    eslint
+        import/prefer-default-export: off
+*/
+
+// const EventEmitter = require('events');
+
+// import logger from '@marcopeg/utils/lib/logger'
+
+class ReduxEvents {
+    constructor () {
+        this.listeners = []
+    }
+
+    registerListener (listeners) {
+        this.listeners = this.listeners.concat(listeners)
+    }
+
+    createReduxMiddleware (ctx) {
+        return store => next => (action) => {
+            // console.log(`EVENT: ${action.type}`)
+            // console.log(listeners)
+            this.listeners
+                .filter(listener => listener.type === action.type)
+                .forEach((listener) => {
+                    try {
+                        if (listener.async) {
+                            setTimeout(() => listener.handler(action)(store.dispatch, store.getState))
+                        } else {
+                            listener.handler(action, ctx)(store.dispatch, store.getState)
+                        }
+                    } catch (e) {
+                        // eslint-disable-next-line
+                        console.error('Redux event handler error')
+                    }
+                })
+
+            return next(action)
+        }
+    }
+}
+
+module.exports = {
+    ReduxEvents,
+}
